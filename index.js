1a05168d68fd75caa0bcb0f1a8a7894c033d0045
simone.rainieri <simone.rainieri@gmail.com>
2018-10-08 13:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+happyHolidays()
+
+function happyHolidaysTo(name) {
+  return `happyHolidaysTo, ${name}!`;
+}
+
+happyHolidaysTo("Simone")
+
+function happyHolidaysTo(holiday, name) {
+  return `happy ${Holiday}, ${name}!`;
+}
+
+happyHolidaysTo("Christmas", "Simone")
+
+function holidayCountdown(holidays, days) {
+  return `It's ${days} days until ${holiday}!`;
+}
+
+holidayCountdown("5", "Christmas")
